28b1178392cd97131e44223915b3b7e30eb1486c
dimant007 <ditryk@gmail.com>
2015-04-24 17:41:35 +0300

    adding code

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
-/**
- * Created by ditry_000 on 23.04.2015.
- */
+function loadData() {
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'data/data.json', true);
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState != 4) return;
+
+        button.parentNode.removeChild(button);
+
+        if (xhr.status != 200) {
+            alert(xhr.status + ': ' + xhr.statusText);
+        } else {
+            try {
+                var data = JSON.parse(xhr.responseText);
+            } catch (e) {
+                alert("Incorrect answer " + e.message);
+            }
+            showData(data);
+        }
+
+    };
+
+    xhr.send();
+
+    button.innerHTML = 'Downloading...';
+    button.disabled = true;
+}
+
+function showData(data) {
+
+    data.forEach(function(dat) {
+        var li = list.appendChild(document.createElement('li'));
+        li.innerHTML = dat.name;
+    });
+
+}
